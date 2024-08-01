5194895b433beaa874c4cea27bb5a0f88509eb92
JuchokJuk <juk.juchok@yandex.ru>
2024-08-01 17:02:04 +0300

    eslint removed

--- src/components/App/Light.tsx
+++ src/components/App/Light.tsx
@@ -1,14 +1,13 @@
-export function Light(){
-    return (
-        <>
-            <ambientLight />
-            <directionalLight
-                position={[5, 10, 15]}
-                castShadow
-                shadow-camera-left={-10}
-                shadow-camera-right={10}
-            />
-        </>
-    );
+export function Light() {
+  return (
+    <>
+      <ambientLight />
+      <directionalLight
+        position={[5, 10, 15]}
+        castShadow
+        shadow-camera-left={-10}
+        shadow-camera-right={10}
+      />
+    </>
+  );
 }
-
